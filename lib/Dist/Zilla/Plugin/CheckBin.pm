a2c21cfe0671484bb00762741d62f44d2441b811
Karen Etheridge <ether@cpan.org>
2014-04-10 19:28:04 -0700

    initial code, docs, tests

--- lib/Dist/Zilla/Plugin/CheckBin.pm
+++ lib/Dist/Zilla/Plugin/CheckBin.pm
@@ -1,31 +1,82 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::CheckBin;
-# ABSTRACT: ...
+# ABSTRACT: Require that our distribution has a particular command available
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with 'Dist::Zilla::Role::InstallTool',
+    'Dist::Zilla::Role::PrereqSource',
+;
+use namespace::autoclean;
 
+sub mvp_multivalue_args { 'command' }
 
-1;
+has command => (
+    isa => 'ArrayRef[Str]',
+    lazy => 1,
+    default => sub { [] },
+    traits => ['Array'],
+    handles => { command => 'elements' },
+);
+
+sub register_prereqs {
+    my $self = shift;
+    $self->zilla->register_prereqs(
+        {
+          phase => 'configure',
+          type  => 'requires',
+        },
+        'Devel::CheckBin' => '0',
+    );
+}
+
+# XXX - this should really be a separate phase that runs after InstallTool -
+# until then, all we can do is die if we are run too soon
+sub setup_installer {
+    my $self = shift;
+
+    my @mfpl = grep { $_->name eq 'Makefile.PL' or $_->name eq 'Build.PL' } @{ $self->zilla->files };
+
+    $self->log_fatal('No Makefile.PL or Build.PL was found. [CheckBin] should appear in dist.ini after [MakeMaker] or variant!') unless @mfpl;
+
+    for my $mfpl (@mfpl)
+    {
+        my $content = "use Devel::CheckBin;\n"
+            . join(' ', map { 'check_bin(\'' . $_ . "\');\n" } $self->command)
+            . "\n";
+        $mfpl->content($content . $mfpl->content);
+    }
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =pod
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::CheckBin;
+In your F<dist.ini>
 
-    ...
+    [CheckBin]
+    command = ls
 
 =head1 DESCRIPTION
 
-...
+L<Dist::Zilla::Plugin::CheckBin> is a L<Dist::Zilla> plugin that modifies the
+F<Makefile.PL> or F<Build.PL> in your distribution to contain a
+L<Devel::CheckBin> call, that asserts that a particular command is available.
+If it is not available, the program exits with a status of zero, which on a
+L<CPAN Testers|cpantesters.org> machine will result in a NA result.
 
-=head1 FUNCTIONS/METHODS
+=for Pod::Coverage mvp_multivalue_args register_prereqs setup_installer
 
-=head2 C<foo>
+=head1 CONFIGURATION
 
-...
+=head2 C<command>
+
+Identifies the name of the command that is searched for. Can be used more than once.
 
 =head1 SUPPORT
 
@@ -35,15 +86,12 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Plugin-CheckBin@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-CheckBin@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Devel::CheckBin>
+* L<Devel::AssertOS> and L<Dist::Zilla::Plugin::AssertOS>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,31 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::CheckBin;
+use Test::DZil;
+use Path::Tiny;
 
-fail('this test is TODO!');
+my $tzil = Builder->from_config(
+    { dist_root => 't/does-not-exist' },
+    {
+        add_files => {
+            path(qw(source dist.ini)) => simple_ini(
+                [ GatherDir => ],
+                [ MakeMaker => ],
+                [ 'CheckBin' => { command => ['ls'] } ],
+            ),
+            path(qw(source lib Foo.pm)) => "package Foo;\n1;\n",
+        },
+    },
+);
+$tzil->build;
+
+my $build_dir = path($tzil->tempdir)->child('build');
+my $file = $build_dir->child('Makefile.PL');
+ok(-e $file, 'Makefile.PL created');
+
+my $content = $file->slurp_utf8;
+unlike($content, qr/[^\S\n]\n/m, 'no trailing whitespace in generated file');
+
+like($content, qr/^use Devel::CheckBin;\n^check_bin\('ls'\);\n/m, 'code inserted into Makefile.PL');
 
 done_testing;
