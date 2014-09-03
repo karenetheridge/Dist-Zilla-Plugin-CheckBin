e7f41741a075aeee90ee3eedf31b2261f6c71e84
Karen Etheridge <ether@cpan.org>
2014-09-02 17:51:21 -0700

    munge the file at the proper phase, if the file was created early enough

--- lib/Dist/Zilla/Plugin/CheckBin.pm
+++ lib/Dist/Zilla/Plugin/CheckBin.pm
@@ -6,7 +6,9 @@ package Dist::Zilla::Plugin::CheckBin;
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
-with 'Dist::Zilla::Role::InstallTool',
+with
+    'Dist::Zilla::Role::FileMunger',
+    'Dist::Zilla::Role::InstallTool',
     'Dist::Zilla::Role::PrereqSource',
 ;
 use Scalar::Util 'blessed';
@@ -46,6 +48,21 @@ sub register_prereqs
     );
 }
 
+my %files;
+sub munge_files
+{
+    my $self = shift;
+
+    my @mfpl = grep { $_->name eq 'Makefile.PL' or $_->name eq 'Build.PL' } @{ $self->zilla->files };
+    for my $mfpl (@mfpl)
+    {
+        $self->log_debug('munging ' . $mfpl->name . ' in file gatherer phase');
+        $files{$mfpl->name} = $mfpl;
+        $self->_munge_file($mfpl);
+    }
+    return;
+}
+
 # XXX - this should really be a separate phase that runs after InstallTool -
 # until then, all we can do is die if we are run too soon
 sub setup_installer
@@ -58,25 +75,34 @@ sub setup_installer
 
     for my $mfpl (@mfpl)
     {
-        my $orig_content = $mfpl->content;
-        $self->log_fatal('could not find position in ' . $mfpl->name . ' to modify!')
-            if not $orig_content =~ m/use strict;\nuse warnings;\n\n/g;
-
-        my $pos = pos($orig_content);
-
-        my $content =
-            "# inserted by " . blessed($self) . ' ' . ($self->VERSION || '<self>') . "\n"
-            . "use Devel::CheckBin;\n"
-            . join('', map { 'check_bin(\'' . $_ . "\');\n" } $self->command)
-            . "\n";
-
-        $mfpl->content(
-            substr($orig_content, 0, $pos)
-            . $content
-            . substr($orig_content, $pos)
-        );
+        next if exists $files{$mfpl->name};
+        $self->log_debug('munging ' . $mfpl->name . ' in setup_installer phase');
+        $self->_munge_file($mfpl);
     }
     return;
+}
+
+sub _munge_file
+{
+    my ($self, $file) = @_;
+
+    my $orig_content = $file->content;
+    $self->log_fatal('could not find position in ' . $file->name . ' to modify!')
+        if not $orig_content =~ m/use strict;\nuse warnings;\n\n/g;
+
+    my $pos = pos($orig_content);
+
+    my $content =
+        "# inserted by " . blessed($self) . ' ' . ($self->VERSION || '<self>') . "\n"
+        . "use Devel::CheckBin;\n"
+        . join('', map { 'check_bin(\'' . $_ . "\');\n" } $self->command)
+        . "\n";
+
+    $file->content(
+        substr($orig_content, 0, $pos)
+        . $content
+        . substr($orig_content, $pos)
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -99,7 +125,7 @@ L<Devel::CheckBin> call, that asserts that a particular command is available.
 If it is not available, the program exits with a status of zero, which on a
 L<CPAN Testers|cpantesters.org> machine will result in a NA result.
 
-=for Pod::Coverage mvp_multivalue_args register_prereqs setup_installer
+=for Pod::Coverage mvp_multivalue_args register_prereqs munge_files setup_installer
 
 =head1 CONFIGURATION OPTIONS
 
